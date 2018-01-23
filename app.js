3dd7f53c40eb4e04924a61bb7c9a3cc6d6ed9473
Benjamin Berman <bdberm@gmail.com>
2018-01-23 10:18:30 -0500

    add morgan logger

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+//logging
+const morgan = require('morgan');
 
 const app = express();
+
+
+// LOGGING
+app.use(morgan('combined'));
 
 app.get("/",(req, res) => res.end('my first express request response cycle!'));
 
