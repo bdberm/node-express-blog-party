068f5aff69e7dbc16b2a1c7ed29c6cf7e1f5435c
Benjamin Berman <bdberm@gmail.com>
2018-01-26 09:59:13 -0500

    iterate through seeds in Index

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require('express');
 //logging
 const morgan = require('morgan');
 
+//File System
+const fs = require('fs');
+
 const app = express();
+
+
 
 
 // LOGGING
@@ -17,6 +22,13 @@ app.use(express.static('assets'));
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get("/", (req, res) => res.render('index',{subHeadVariable: "subhead from var"}));
+// app.get("/", (req, res) => res.render('index',{subHeadVariable: "subhead from var"}));
+app.get("/", (req, res) => {
+  const dogs = fs.readFileSync('db/dog_seeds.json', 'utf-8');
+  const dogArr = JSON.parse(dogs);
+
+  res.render('index',{subHeadVariable: "subhead from var", dogArr: dogArr});
+});
+
 
 app.listen(3000, () => console.log('listening on port 3000'));
