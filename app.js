bba2169b6935197a5333d1c2379e598a25955565
Benjamin Berman <bdberm@gmail.com>
2018-01-26 09:08:39 -0500

    add layout.pug and assets folder

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ const app = express();
 // LOGGING
 app.use(morgan('combined'));
 
+// For servinc static files like CSS and JS
+app.use(express.static('assets'));
+
+
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-// app.get("/",(req, res) => res.end('my first express request response cycle!'));
-app.get("/", (req, res) => res.render('hello',{subHeadVariable: "subhead from var"}));
+app.get("/", (req, res) => res.render('index',{subHeadVariable: "subhead from var"}));
 
 app.listen(3000, () => console.log('listening on port 3000'));
